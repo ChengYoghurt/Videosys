1ea3d7c17ab3174daeadef1f90a90a4effb3017e
ChengYoghurt <1351425316@qq.com>
2025-02-18 15:15:40 +0800

    Prepared training on 29x720p

--- examples/open_sora_plan/EvolutionSearcher.py
+++ examples/open_sora_plan/EvolutionSearcher.py
@@ -52,12 +52,12 @@ choice = lambda x: x[np.random.randint(len(x))] if isinstance(
 # safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
 
 # 4s
-prompts = [
-    "a muffin with a burning candle and a love sign by a ceramic mug", # food
-    "a group of friend place doing hand gestures of agreement", # humannvi
-    "aerial view of snow piles", # scenery
-    "yacht sailing through the ocean", # vehicle
-]
+# prompts = [
+#     "a muffin with a burning candle and a love sign by a ceramic mug", # food
+#     "a group of friend place doing hand gestures of agreement", # humantmux
+#     "aerial view of snow piles", # scenery
+#     "yacht sailing through the ocean", # vehicle
+# ]
 
 # 1s
 # prompts = [
@@ -66,6 +66,14 @@ prompts = [
 #     "freshly baked finger looking cookies", # food
 #     "people carving a pumpkin", # human
 # ]
+
+# 29x720p
+prompts = [
+    "a muffin with a burning candle and a love sign by a ceramic mug", # food
+    "a group of friend place doing hand gestures of agreement", # human
+    "eiffel tower from the seine river", # archi
+    "yacht sailing through the ocean", # vehicle
+]
 
 def load_ref_videos(ref_videos_folder):
     ref_videos = []
--- examples/open_sora_plan/sample_ea.py
+++ examples/open_sora_plan/sample_ea.py
@@ -1,19 +1,17 @@
 from videosys import OpenSoraPlanConfig, VideoSysEngine
-prompts = [
+prompts_1s = [
     "a black dog wearing halloween costume", # animal
     "an apartment building with balcony", # archi
     "freshly baked finger looking cookies", # food
     "people carving a pumpkin", # human
-    "scenic video of sunset", # scenery
+    # "scenic video of sunset", # scenery
 ]
 
-sora_prompts = [
-    "A stylish woman walks down a Tokyo street filled with warm glowing neon and animated city signage. She wears a black leather jacket, a long red dress, and black boots, and carries a black purse. She wears sunglasses and red lipstick. She walks confidently and casually. The street is damp and reflective, creating a mirror effect of the colorful lights. Many pedestrians walk about.",
-    "Several giant wooly mammoths approach treading through a snowy meadow, their long wooly fur lightly blows in the wind as they walk, snow covered trees and dramatic snow capped mountains in the distance, mid afternoon light with wispy clouds and a sun high in the distance creates a warm glow, the low camera view is stunning capturing the large furry mammal with beautiful photography, depth of field.",
-    "A movie trailer featuring the adventures of the 30 year old space man wearing a red wool knitted motorcycle helmet, blue sky, salt desert, cinematic style, shot on 35mm film, vivid colors.",
-    "Drone view of waves crashing against the rugged cliffs along Big Sur’s garay point beach. The crashing blue waters create white-tipped waves, while the golden light of the setting sun illuminates the rocky shore. A small island with a lighthouse sits in the distance, and green shrubbery covers the cliff’s edge. The steep drop from the road down to the beach is a dramatic feat, with the cliff’s edges jutting out over the sea. This is a view that captures the raw beauty of the coast and the rugged landscape of the Pacific Coast Highway.",
-    "Animated scene features a close-up of a short fluffy monster kneeling beside a melting red candle. The art style is 3D and realistic, with a focus on lighting and texture. The mood of the painting is one of wonder and curiosity, as the monster gazes at the flame with wide eyes and open mouth. Its pose and expression convey a sense of innocence and playfulness, as if it is exploring the world around it for the first time. The use of warm colors and dramatic lighting further enhances the cozy atmosphere of the image.",
-    "A gorgeously rendered papercraft world of a coral reef, rife with colorful fish and sea creatures.",
+prompts_4s = [
+    "a muffin with a burning candle and a love sign by a ceramic mug", # food
+    "a group of friend place doing hand gestures of agreement", # human
+    "aerial view of snow piles", # scenery
+    "yacht sailing through the ocean", # vehicle
 ]
 
 def save_ref_video(video, i, prompt, engine):
@@ -23,7 +21,7 @@ def save_ref_video(video, i, prompt, engine):
     if not isinstance(video, torch.Tensor):
         # Convert to a PyTorch tensor if it's not already one
         video = torch.tensor(video)
-    ref_video_folder = f"/home/yfeng/ygcheng/src/VideoSys/examples/open_sora_plan/assets/category_ref_videos_4s"
+    ref_video_folder = f"examples/open_sora_plan/assets/29x720p"
     # Save the video tensor to a .pt file
 
     ref_video_path = os.path.join(ref_video_folder, f"{i}.pt")
@@ -35,7 +33,7 @@ def run_base(save_ref_videos=False, load_ea_timesteps=False):
     # open-sora-plan v1.2.0
     # transformer_type (len, res): 93x480p 93x720p 29x480p 29x720p
     # change num_gpus for multi-gpu inference
-    config = OpenSoraPlanConfig(version="v120", transformer_type="93x480p", num_gpus=1)
+    config = OpenSoraPlanConfig(version="v120", transformer_type="29x720p", num_gpus=1)
     engine = VideoSysEngine(config)
 
     ea_timesteps_list = []
@@ -43,17 +41,22 @@ def run_base(save_ref_videos=False, load_ea_timesteps=False):
     # seed=-1 means random seed. >0 means fixed seed.
     # File path
     prompt_file_path = "/home/yfeng/ygcheng/src/Open-Sora/assets/texts/t2v_sora.txt"
+    # prompt_file_path = "/home/yfeng/ygcheng/src/VBench/prompts/all_category.txt"
 
     # Read all prompts
     with open(prompt_file_path, "r") as f:
-        prompts = [line.strip() for line in f.readlines()]
-
+        # prompts = [line.strip() for line in f.readlines()]
+        prompts = [line.strip() for i, line in enumerate(f.readlines()) if i % 16 == 0]
+    prompts = [
+    "aerial view of snow piles", # scenery
+    "yacht sailing through the ocean", # vehicle
+    ]
     for i, prompt in enumerate(prompts):
 
         if load_ea_timesteps:
             import yaml
             # Load YAML file
-            ea_timesteps_path = "/home/yfeng/ygcheng/src/VideoSys/examples/open_sora_plan/outputs/93x480p_step40_search100_category/ea_timesteps.yaml"
+            ea_timesteps_path = "examples/open_sora_plan/outputs/93x480p_step40_search100_category/ea_timesteps.yaml"
             with open(ea_timesteps_path, "r") as file:
                 ea = yaml.safe_load(file)  # Use safe_load to avoid execution risks
 
@@ -97,11 +100,11 @@ def run_base(save_ref_videos=False, load_ea_timesteps=False):
                 save_ref_video(video, i, prompt, engine)
 
             prompt_suffix = prompt[:20] if len(prompt) > 20 else prompt
-            engine.save_video(video, f"./outputs/category_4s_org_osp120/{prompt_suffix}_4s.mp4")
+            engine.save_video(video, f"./outputs/category_29x720p_org/{prompt_suffix}.mp4")
 
 
 
 
 if __name__ == "__main__":
-    run_base(save_ref_videos=False, load_ea_timesteps=True)
+    run_base(save_ref_videos=True, load_ea_timesteps=False)
 
